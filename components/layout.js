db5f3378d2a2fb708b4651b59e4efdf1492ef310
Danny Wilson <dannywilson32@gmail.com>
2021-04-30 14:45:54 +0100

    fixed fullscreen bg colour if page spills the fold

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,8 @@
 export default function Layout({ children }) {
   return (
     <>
-      <main class="bg-yellow-50 h-screen p-4 md:p-8">
-      { children }
+      <main className="bg-yellow-50 h-full min-h-full p-4 md:p-8">
+        { children }
       </main>
     </>
   )
