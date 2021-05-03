3a8f43420074b70868ba26ccccbf02d0df4d3edf
Danny Wilson <dannywilson32@gmail.com>
2021-05-03 17:12:46 +0100

    sort latest posts by date desc

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import Contact from '../components/contact'
 import PostList from '../components/PostList/postList'
 
 export default function Home({posts}) {
-  console.log(posts)
   return (
     <Layout>
       <Head>
@@ -24,7 +23,7 @@ export default function Home({posts}) {
 }
 
 export async function getStaticProps() {
-  const query = '*[_type == "post" && !(_id in path("drafts.**"))] {title, slug, publishedAt}'
+  const query = '*[_type == "post" && !(_id in path("drafts.**"))] | order(publishedAt desc) {title, slug, publishedAt}'
   const data = await client.fetch(query)
 
   return {
